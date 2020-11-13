919780fd987f66c2b5c1dbfdc7c474acf5dc9d3a
Guevara Villegas <rguevarav@migraciones.gob.pe>
2020-11-13 18:36:42 -0500

    ver 1.0 13/11/2020

---
+++ SIDTEFIM/insert.sql
@@ -0,0 +1,21 @@
+﻿
+/*
+	► INSERT: SidtefimModulo */
+INSERT INTO SidtefimModulo
+	VALUES
+	(
+		'SITRAN', 1, 'Sistema de Trazabilidad de Nacionalización', 'Sistema de Trazabilidad de Nacionalización', '1.0'
+	)
+
+
+/*
+	► INSERT: SidtefimProcedimiento
+	► SELECT * FROM SidtefimProcedimiento */
+INSERT INTO SidtefimProcedimiento(bActivo, sIcono, sNombre, sRuta, sIdModulo) VALUES(1,'AddBox','PARA EVALUACIÓN','/test','SITRAN')
+INSERT INTO SidtefimProcedimiento(bActivo, sIcono, sNombre, sRuta, sIdModulo) VALUES(1,'Check','EN PROYECTO EVALUACIÓN','/test','SITRAN')
+INSERT INTO SidtefimProcedimiento(bActivo, sIcono, sNombre, sRuta, sIdModulo) VALUES(1,'Clear','NO LEIDOS','/test','SITRAN')
+INSERT INTO SidtefimProcedimiento(bActivo, sIcono, sNombre, sRuta, sIdModulo) VALUES(1,'DeleteOutline','REGISTRAR PROCEDIMIENTO','/test','SITRAN')
+INSERT INTO SidtefimProcedimiento(bActivo, sIcono, sNombre, sRuta, sIdModulo) VALUES(1,'ChevronRight','PARA ASIGNAR','/test','SITRAN')
+INSERT INTO SidtefimProcedimiento(bActivo, sIcono, sNombre, sRuta, sIdModulo) VALUES(1,'Edit','CONSULTAS','/test','SITRAN')
+
+
---
+++ SIDTEFIM/query.sql
@@ -0,0 +1,11 @@
+
+/*» INSERT: SidtefimModulo */
+INSERT INTO SidtefimModulo
+	VALUES
+	(
+		'SITRAN', 1, 'Sistema de Trazabilidad de Nacionalización', 'Sistema de Trazabilidad de Nacionalización', '1.0'
+	)
+
+
+/*» INSERT: SidtefimModulo */
+SidtefimProcedimiento
